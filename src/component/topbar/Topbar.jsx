b43906204f0466d8777f4e2b939986af9f7d173e
Biraj Pokharel <buraj.sainik@gmail.com>
2021-08-23 23:50:59 +0545

    Appbar created

--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -1,5 +1,30 @@
 import './topbar.scss';
-
+import { Person, Mail } from '@material-ui/icons';
 export default function Topbar() {
-  return <div className="topbar">Topbar</div>;
+  return (
+    <div className="topbar ">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Genious
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>= 445 55 11</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>bisk@gmail.com </span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
