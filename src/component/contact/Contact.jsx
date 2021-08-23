94c95d01e8720fa4bcd3fb30132598404cfc8c3d
Biraj Pokharel <buraj.sainik@gmail.com>
2021-08-23 22:11:28 +0545

    section seperated

--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './Contact.scss';
 
 export default function Contact() {
-  return <div className="contact">Contact</div>;
+  return <div className="contact"></div>;
 }
--- src/component/works/Works.jsx
+++ src/component/works/Works.jsx
@@ -1,5 +1,5 @@
 import './Works.scss';
 
 export default function Works() {
-  return <div>Works</div>;
+  return <div className="works">Works</div>;
 }
