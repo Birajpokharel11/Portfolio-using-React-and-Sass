ddad91ea42e2d95dedaf29c15e094670b21b6724
Biraj Pokharel <54663280+Birajpokharel11@users.noreply.github.com>
2021-08-24 15:27:02 +0545

    Portfolio added

--- src/component/intro/Intro.jsx
+++ src/component/intro/Intro.jsx
@@ -1,10 +1,37 @@
 import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ['Developer!', 'Designer', 'MERN Stack']
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      <div className="left">LEFT</div>
-      <div className="right">RIGHT</div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Biraj Pokharel</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/component/portfolio/Portfolio.jsx
+++ src/component/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './Portfolio.scss';
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <ui>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ui>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Banking App </h3>
+        </div>
+      </div>
     </div>
   );
 }
